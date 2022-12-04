0ef7b92eb3cdf400ea47ae8e78abb91bb9b1ff5f
tracer-ics2022 <tracer@njuics.org>
2022-12-04 20:44:24 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:44:24 up 51 min,  4 users,  load average: 0.23, 0.20, 0.18

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -23,7 +23,7 @@ extern void __am_asm_trap(void);
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
-
+	putch('l');
   // register event handler
   user_handler = handler;
 

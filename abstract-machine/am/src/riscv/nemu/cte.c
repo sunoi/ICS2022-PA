d60224c3ac07f72630bc01dd072172d3ad6392df
tracer-ics2022 <tracer@njuics.org>
2022-12-05 19:30:30 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:30:30 up 53 min,  4 users,  load average: 0.16, 0.17, 0.18

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,7 +7,6 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-		printf("jiba");
     switch (c->mcause) {
 			case 0xb:
 				ev.event = EVENT_YIELD;

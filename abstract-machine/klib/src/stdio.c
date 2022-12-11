0d5d8f2bd9a15daa81aa116efea161a905730b33
tracer-ics2022 <tracer@njuics.org>
2022-12-12 01:32:50 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:32:50 up  5:25,  4 users,  load average: 0.23, 0.09, 0.03

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -62,7 +62,7 @@ static int make_out(char *out, const char *fmt, va_list ap) {
 }
 
 int printf(const char *fmt, ...) {
-  char out[120000];
+  char out[120010];
 	va_list ap;
 	va_start(ap, fmt);
 	make_out(out, fmt, ap);

12bc4908a562e2e6548cd8e46762b54949b40eb2
tracer-ics2022 <tracer@njuics.org>
2022-12-12 19:40:47 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:40:47 up 34 min,  4 users,  load average: 0.28, 0.21, 0.18

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -21,12 +21,12 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
 	Elf_Phdr phdr[phnum];
 	ramdisk_read(&phdr, ehsize, sizeof(phdr)*phnum);
-	for (size_t i = 0; i < phnum; i++) {
+	/*for (size_t i = 0; i < phnum; i++) {
 		if (phdr[i].p_type == PT_LOAD) {
-			//ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
-			//memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz-phdr[i].p_filesz);
+			ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+			memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz-phdr[i].p_filesz);
 		}
-	}
+	}*/
   return ehdr.e_entry;
 }
 

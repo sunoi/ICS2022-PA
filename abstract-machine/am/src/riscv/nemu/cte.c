8b48c5e5314963651fdb9828e72a155e876553aa
tracer-ics2022 <tracer@njuics.org>
2022-12-12 20:49:07 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:49:07 up  1:43,  4 users,  load average: 0.11, 0.12, 0.09

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -11,6 +11,8 @@ Context* __am_irq_handle(Context *c) {
 			case -1:
 				ev.event = EVENT_YIELD;
 				break;
+			case 2:
+				ev.event = EVENT_SYSCALL;
       default: ev.event = EVENT_ERROR; break;
     }
 

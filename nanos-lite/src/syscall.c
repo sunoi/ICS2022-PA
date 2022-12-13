1e7f92f973590609fd698dc6e193b827b4523727
tracer-ics2022 <tracer@njuics.org>
2022-12-13 23:35:13 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:35:13 up  2:20,  8 users,  load average: 0.62, 0.36, 0.28

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -19,7 +19,7 @@ void sys_write(Context *c) {
 		size_t len = c->GPR4;
 		//printf("len=%d\n", len);
 		for (size_t i = 0; i < len+10; i++) {
-			putch((char)(buf));
+			putch((char)('a'+buf));
 			buf++;
 		}
 		c->GPRx = len;

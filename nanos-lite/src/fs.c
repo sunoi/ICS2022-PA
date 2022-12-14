4ee19f1280e0e7dc571d163a26929e7072bc1842
tracer-ics2022 <tracer@njuics.org>
2022-12-14 20:41:30 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:41:30 up  6:16,  5 users,  load average: 0.37, 0.34, 0.23

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -56,6 +56,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
 	size_t open_offset = file_table[fd].open_offset;
 	size_t size = file_table[fd].disk_offset;
 	
+	printf("%d,%d,%d\n", disk_offset, open_offset, size);
 	if (len > size - open_offset) 
 		len = size - open_offset;
 

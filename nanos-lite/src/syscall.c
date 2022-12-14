73d0a8c26619efb8d320ceccf81779e4d9efd2ee
tracer-ics2022 <tracer@njuics.org>
2022-12-14 20:06:03 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:06:03 up  5:40,  5 users,  load average: 0.04, 0.09, 0.15

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,12 @@
 #include <common.h>
 #include "syscall.h"
 
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+
 void sys_yield(Context *c) {
 	yield();
 	c->GPRx = 0;
@@ -32,6 +38,33 @@ void sys_brk(Context *c) {
 	return;
 }
 
+void sys_open(Context *c) {
+	const char* pathname = (const char*)c->GPR2;
+	int flags = c->GPR3;
+	int mode = c->GPR4;
+	c->GPRx = fs_open(pathname, flags, mode);
+}
+
+void sys_read(Context *c) {
+	int fd = c->GPR2;
+	int count = c->GPR4;
+	char *buf = (char*)c->GPR3;
+	c->GPRx = fs_read(fd, (void*)buf, count);
+
+}
+
+void sys_lseek(Context *c) {
+	int fd = c->GPR2;
+	int offset = c->GPR3;
+	int whence = c->GPR4;
+
+	c->GPRx = fs_lseek(fd, offset, whence);
+}
+
+void sys_close(Context *c) {
+	int fd = c->GPR2;
+	c->GPRx = fs_close(fd);
+}
 
 void do_syscall(Context *c) {
   uintptr_t a[4];

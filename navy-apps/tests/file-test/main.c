4e5b5c0a52ae675b21f365f5a48b5f3a39721a7b
tracer-ics2022 <tracer@njuics.org>
2022-12-15 20:35:04 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:35:04 up  3:34,  5 users,  load average: 1.23, 0.67, 0.69

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -15,12 +15,13 @@ int main() {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+	
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 	
+	fseek(fp, ftell(fp), SEEK_SET);
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);

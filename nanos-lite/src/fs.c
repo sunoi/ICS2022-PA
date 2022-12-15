9bf72cdc70d05051de73fccc3edc0cf65b9bdc57
tracer-ics2022 <tracer@njuics.org>
2022-12-15 17:35:27 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:35:27 up 35 min,  4 users,  load average: 2.07, 1.14, 0.73

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -79,6 +79,7 @@ size_t fs_write(int fd, const void *buf, size_t len) {
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
+	printf("%d\n", whence);
 	switch (whence) {
 		case SEEK_SET:
 			file_table[fd].open_offset = offset;

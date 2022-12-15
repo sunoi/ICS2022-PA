bf0c4a797f0ca15c40668f9a44258d9477c6da18
tracer-ics2022 <tracer@njuics.org>
2022-12-15 17:25:11 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:25:11 up 24 min,  4 users,  load average: 0.48, 0.61, 0.46

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -47,6 +47,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 			memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz-phdr[i].p_filesz);
 		}
 	}
+	printf("fd=%d\n", fd);
 	fs_close(fd);
 	return ehdr.e_entry;
 }

259de86b92c6752c9faf2ff5656e7a3f9af63a41
tracer-ics2022 <tracer@njuics.org>
2022-12-15 19:03:14 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:03:14 up  2:02,  5 users,  load average: 0.36, 0.45, 0.59

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -61,7 +61,8 @@ size_t fs_read(int fd, void *buf, size_t len) {
 	
 	//printf("read_offset=%d\n", open_offset);
 	//printf("fs_read = %d,%d,%d\n", disk_offset, open_offset, size);
-	if (len > size - open_offset){ 
+	if (len > size - open_offset){
+		printf("len=%d\n", len);
 		len = size - open_offset;
 		printf("jile\n");
 	}

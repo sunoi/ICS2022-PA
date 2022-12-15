974bc810f612e6a974aa44cdfe397200a2e6417e
tracer-ics2022 <tracer@njuics.org>
2022-12-15 21:24:45 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:24:45 up  4:24,  5 users,  load average: 0.46, 0.65, 0.65

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -70,19 +70,6 @@ size_t fs_read(int fd, void *buf, size_t len) {
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
-	/*if (file_table[fd].write != NULL) {
-		file_table[fd].write(buf, file_table[fd].open_offset, len);
-	}*/
-	/*
-	if (fd == 1 || fd == 2) {
-		for (size_t i = 0; i < len; i++) {
-			putch((char)*buf);
-			buf++;
-		}
-		file_table[fd].open_offset += len;
-		return len;
-	}*/
-
 	size_t disk_offset = file_table[fd].disk_offset;
 	size_t open_offset = file_table[fd].open_offset;
 	size_t size = file_table[fd].size;

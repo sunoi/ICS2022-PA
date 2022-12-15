3a1e1fdde0c025351feccffef488d43eeee1a640
tracer-ics2022 <tracer@njuics.org>
2022-12-15 19:25:53 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:25:53 up  2:25,  5 users,  load average: 0.27, 0.47, 0.53

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -20,8 +20,9 @@ int main() {
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-	size = ftell(fp);
-	printf("%ld\n", size);
+	
+	fseek(fp, 0, SEEK_CUR);	
+
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);

e4e98e806f4350e8402b92b8b03b77f1c048ef9d
tracer-ics2022 <tracer@njuics.org>
2022-12-16 21:08:02 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:08:02 up  5:48,  5 users,  load average: 0.44, 0.37, 0.31

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -50,6 +50,7 @@ void init_fs() {
 
 int fs_open(const char *pathname, int flags, int mdoe) {
 	for (int i = 0; i < file_number; i++) {
+		printf("%d\n", i);
 		if (strcmp(pathname, file_table[i].name) == 0) {
 			return i;
 		}

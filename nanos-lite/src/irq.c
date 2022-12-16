7eadb5a8c82da644d61b378c22c1c375739be90b
tracer-ics2022 <tracer@njuics.org>
2022-12-16 15:42:06 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:42:06 up 22 min,  4 users,  load average: 0.83, 0.53, 0.24

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -9,6 +9,7 @@ static Context* do_event(Event e, Context* c) {
 	switch (e.event) {
 		case EVENT_YIELD:
 			Log("Trriger yield.");
+			c->mepc += 4;
 			break;
 		case EVENT_SYSCALL:
 			//Log("Trriger syscall");

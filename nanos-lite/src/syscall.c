86b04315bd79bbad3d72c67a6c96f971b9b4d5c9
tracer-ics2022 <tracer@njuics.org>
2022-12-16 20:40:40 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:40:40 up  5:20,  4 users,  load average: 0.25, 0.22, 0.13

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -73,7 +73,7 @@ void sys_gettimeofday(Context *c) {
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-	//printf("a[0]=%d\n", a[0]);
+	printf("a[0]=%d\n", a[0]);
   switch (a[0]) {
 		case 0:
 			sys_exit(c);break;

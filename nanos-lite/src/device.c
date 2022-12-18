b93fb9f9bd6fc8258ed4e08f0886cd40b36e13c1
tracer-ics2022 <tracer@njuics.org>
2022-12-18 21:37:21 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:37:21 up  4:30,  5 users,  load average: 0.28, 0.31, 0.33

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -37,7 +37,6 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-	assert(0);
 	int width = io_read(AM_GPU_CONFIG).width;
 	int height = io_read(AM_GPU_CONFIG).height;
 	sprintf(buf, "width=%d, height=%d", width, height);

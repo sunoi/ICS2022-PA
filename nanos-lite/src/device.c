3477d34174eb91751d73f9dfaac21bb52eba82ec
tracer-ics2022 <tracer@njuics.org>
2022-12-20 16:43:44 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:43:44 up 10 min,  4 users,  load average: 0.23, 0.13, 0.06

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -50,7 +50,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 	int y = t / width;
 	int x = t % width;
 
-	//printf("x=%d, y=%d\n", x, y);
+	printf("x=%d, y=%d\n", x, y);
 	
 	io_write(AM_GPU_FBDRAW, x, y, (void*)buf, len / 4, 1, true);
 	return 0;

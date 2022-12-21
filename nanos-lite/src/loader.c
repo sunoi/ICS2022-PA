99a55057d1a75e539ab30c78998464f93479e55a
tracer-ics2022 <tracer@njuics.org>
2022-12-21 16:55:42 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:55:42 up  6:49,  4 users,  load average: 0.27, 0.44, 0.43

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,6 +24,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 	Elf_Ehdr ehdr;
 	//ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
 	int fd = fs_open(filename, 0, 0);
+	printf("fd=%d\n", fd);
 	if (fd < 0) assert(0);
 	fs_lseek(fd, 0, SEEK_SET);
 	fs_read(fd, &ehdr, sizeof(Elf_Ehdr));

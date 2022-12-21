61480f56dc45fb6b8609178f29a37b3773303d61
tracer-ics2022 <tracer@njuics.org>
2022-12-21 19:32:32 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:32:32 up  9:26,  5 users,  load average: 0.27, 0.37, 0.46

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -3,11 +3,12 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
-	//printf("reach blitsurface\n");
+	printf("reach blitsurface\n");
 	int src_x, src_y, src_w, src_h;
 	if (srcrect == NULL) {
 		src_x = 0;
@@ -108,7 +109,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 		w = s->w;
 		h = s->h;
 	}
-
+	
 	if (s->format->BitsPerPixel == 32) {
 		NDL_DrawRect((uint32_t*)s->pixels, x, y, w, h);
 	}

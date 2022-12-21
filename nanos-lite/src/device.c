90e2acda09acaed9a802b8dd76a9210741ffccde
tracer-ics2022 <tracer@njuics.org>
2022-12-21 19:04:26 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:04:26 up  8:58,  5 users,  load average: 1.06, 0.81, 0.73

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,7 +25,6 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-	assert(0);
 	if (ev.keycode == AM_KEY_NONE) return 0;
 
 	if (ev.keydown == true) {

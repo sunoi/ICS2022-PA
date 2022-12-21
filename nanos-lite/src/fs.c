fa147a66ed495044f019fcbf101cf513ac7975c8
tracer-ics2022 <tracer@njuics.org>
2022-12-21 19:03:28 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:03:28 up  8:57,  5 users,  load average: 2.20, 0.92, 0.76

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -63,6 +63,7 @@ int fs_open(const char *pathname, int flags, int mdoe) {
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
+	printf("fd=%d\n", fd);
 	if (file_table[fd].read != NULL)
 		return file_table[fd].read(buf, file_table[fd].open_offset, len);
 

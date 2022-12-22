64cd4f0d8b47efad7e8e3d11bcb412ebaaf76b61
tracer-ics2022 <tracer@njuics.org>
2022-12-22 13:39:32 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:39:32 up  2:18,  5 users,  load average: 0.34, 0.77, 0.77

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -101,7 +101,6 @@ int main(int argc, char *argv[], char *envp[]) {
     }
 
     if (i != -1 && i <= i_max) {
-			printf("i=%d\n", i);
       i += page * 10;
       auto *item = &items[i];
       const char *exec_argv[3];
@@ -110,6 +109,7 @@ int main(int argc, char *argv[], char *envp[]) {
       exec_argv[2] = NULL;
       clear_display();
       SDL_UpdateRect(screen, 0, 0, 0, 0);
+			printf("reach here\n");
       execve(exec_argv[0], (char**)exec_argv, (char**)envp);
       fprintf(stderr, "\033[31m[ERROR]\033[0m Exec %s failed.\n\n", exec_argv[0]);
     } else {
